bbd6c9e419f6b453b6395ce90d9075f607d31962
Lucas Flores <luflores@k1r2p3.42mulhouse.fr>
2025-12-08 15:09:12 +0100

    GNL

---
+++ get_next_line.c
@@ -0,0 +1,139 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luflores <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/06 15:26:58 by luflores          #+#    #+#             */
+/*   Updated: 2025/11/06 15:27:00 by luflores         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*mfunc(char *a, char *b, char f)
+{
+	char	*tmp;
+
+	if (f == 'S')
+	{
+		tmp = ft_strjoin(a, b);
+		free(a);
+		return (tmp);
+	}
+	else if (f == 'F')
+	{
+		free(b);
+		free(a);
+		return (NULL);
+	}
+	return (NULL);
+}
+
+static char	*lookforline(char **buf)
+{
+	char	*nstr;
+	char	*tmp;
+	char	*nl_pos;
+
+	nl_pos = ft_strchr(*buf, '\n');
+	if (nl_pos)
+	{
+		nstr = ft_strdup(*buf, '\n');
+		tmp = ft_strdup(nl_pos + 1, '\0');
+		free(*buf);
+		*buf = tmp;
+		return (nstr);
+	}
+	nstr = ft_strdup(*buf, '\0');
+	free(*buf);
+	*buf = NULL;
+	return (nstr);
+}
+
+static char	*readintobuffer(char *b, int fd)
+{
+	int		ret;
+	char	*buf;
+
+	if (!b)
+		b = ft_calloc(1, 1);
+	if (!b)
+		return (NULL);
+	ret = 1;
+	buf = malloc(BUFFER_SIZE + 1);
+	if (!buf)
+		return (NULL);
+	while (ret > 0)
+	{
+		ret = read(fd, buf, BUFFER_SIZE);
+		if (ret < 0)
+			return (mfunc(b, buf, 'F'));
+		buf[ret] = 0;
+		b = mfunc(b, buf, 'S');
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	free(buf);
+	return (b);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buf[1024];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		if (buf[fd])
+			free(buf[fd]);
+		buf[fd] = NULL;
+		return (NULL);
+	}
+	buf[fd] = readintobuffer(buf[fd], fd);
+	if (!buf[fd] || buf[fd][0] == '\0')
+	{
+		if (buf[fd])
+			free(buf[fd]);
+		buf[fd] = NULL;
+		return (NULL);
+	}
+	line = lookforline(&buf[fd]);
+	return (line);
+}
+
+// int main(void)
+// {
+// 	int fd;
+// 	int fd3;
+// 	fd = open("get_next_line.c", O_RDONLY);
+// 	char *line1 = ft_calloc(1,1);
+// 	while (line1 != NULL)
+// 	{
+// 		if (line1 != NULL)
+// 			free(line1);
+// 		line1 = (get_next_line(fd));
+// 		printf("line: %s", line1);	
+// 	}
+// 	if (line1)
+// 		free(line1);
+// 	return(0);
+// }
+
+// int main(void)
+// {
+// 	int fd;
+// 	fd = open("get_next_line.c", O_RDONLY);
+// 	char *line1 = ft_calloc(1,1);
+// 	while (line1 != NULL)
+// 	{
+// 		if (line1 != NULL)
+// 			free(line1);
+// 		line1 = (get_next_line(fd));
+// 		printf("line: %s", line1);	
+// 	}
+// 	if (line1)
+// 		free(line1);
+// 	return(0);
+// }
